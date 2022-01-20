40e1aba490c1c756edc8980e15bdacd2a80d4928
Ivan <i.g.popov@hotmail.com>
2022-01-20 21:48:57 +0200

    initial commit

--- 01. CSharp-Fundamentals-Intro-and-Basic-Syntax-Exercise/10. Rage Expenses/Program.cs
+++ 01. CSharp-Fundamentals-Intro-and-Basic-Syntax-Exercise/10. Rage Expenses/Program.cs
@@ -6,7 +6,19 @@ namespace _10._Rage_Expenses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int lostGames = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardtPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            int trashHeadsets = lostGames/2;
+            int trashMouse = lostGames/3;
+            int trashKeyboard = lostGames/6;
+            int trashDisplay = lostGames/12;
+
+            double expenses = (trashHeadsets*headsetPrice)+(trashMouse*mousePrice)+(trashKeyboard*keyboardtPrice)+(trashDisplay*displayPrice);
+            Console.WriteLine($"Rage expenses: {expenses:f2} lv.");
         }
     }
 }
